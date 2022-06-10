8fe5fc6974f54c7da8f8f36efcd0b4c186446dd4
Nathani Luving <cosmasnathan9@gmail.com>
2022-06-10 23:56:16 +0300

    interest commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// function to compute interest
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // principal validation for negative value
+    if (principal <= 0){
+        alert("Enter a positive number!");
+       
+            document.getElementById("principal").focus();
+    }
+    else {
+
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit " + '<span class="highlight">'+principal+'</span>'+ ",\<br\>at an interest rate of "+'<span class="highlight">'+rate+'</span>' +"%\<br\>You will receive an amount of "+'<span class="highlight">'+interest+'</span>' +",\<br\>in the year "+'<span class="highlight">'+year+'</span>'+"\<br\>"
+
+    }
+    
+}
+
+// READING THE RANGE VALUE
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
